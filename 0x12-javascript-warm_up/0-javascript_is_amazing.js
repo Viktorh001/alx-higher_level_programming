3bde42ee212901fa6bdc25372fb2e834eae7dd49
udoka <ezuvictor@gmail.com>
2024-04-15 11:34:22 -0700

    update

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 //Write a script that prints “JavaScript is amazing”
 const MyVar = "JavaScript is amazing";
 console.log(MyVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 //Write a script that prints 3 lines:
 const c = "C is fun";
 const py = "Python is cool";
